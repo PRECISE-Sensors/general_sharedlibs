35f9b030953d25dd8d9c7c9ad41c8ae593c88d64
Yana Salchak <58014439+yasalchak@users.noreply.github.com>
2025-01-21 14:34:03 +1000

    Update main.cpp

--- examples/HW_Timer/src/main.cpp
+++ examples/HW_Timer/src/main.cpp
@@ -1,11 +1,18 @@
 
 #include "ESP_HW_Timer.h"
 
-TimerHW timer(1000, 1); // 1ms interval, Timer1
+
+//Static member variables must be defined outside the class. /*required because static variables are shared across all instances of the class*/
+//volatile std::atomic<bool> TimerHW::timer0_flag{false};
+volatile std::atomic<bool> TimerHW::timer1_flag{false};
+
+//Single instance of TimerHW (only one should exist)
+TimerHW timerInt(1000, 1); // 1ms interval, Timer1
 
 void setup() {
   Serial.begin(921600);
-  timer.begin(); // Start the timer
+  timerInt.begin();           // Start the timer interrupt
+  delay(10);
 }
 
 void loop() {
